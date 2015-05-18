441e0d01d45644942ff96528205792957ea710e2
Devan <devanoatney@gmail.com>
2015-05-18 13:39:42 -0700

    Added more UI to the merchant screen.

--- Game/Assets/Scripts/Towns/NPC_ArmorMerchantScript.cs
+++ Game/Assets/Scripts/Towns/NPC_ArmorMerchantScript.cs
@@ -10,7 +10,7 @@ public class NPC_ArmorMerchantScript : NPCScript
 	public TextAsset m_taWares;
 	List<MerchantItem> m_lItems = new List<MerchantItem>();
 	DCScript dc;
-
+	Vector2 m_vScrollPosition = new Vector2(0,0);
 	class MerchantItem
 	{
 		public string m_szItemName = "";
@@ -32,7 +32,6 @@ public class NPC_ArmorMerchantScript : NPCScript
 	{
 		m_lItems.Clear();
 		string[] itemLines = m_taWares.text.Split('\n');
-		Debug.Log(itemLines);
 		foreach(string item in itemLines)
 		{
 			string[] piece = item.Split(',');
@@ -67,13 +66,43 @@ public class NPC_ArmorMerchantScript : NPCScript
 	{
 		if(m_bShowScreen == true)
 		{
-			GUI.Box(new Rect(0, 0, 200, 200), "");
-			float yPos = 0;
+			float screenHeight = Screen.height;
+			float screenWidth = Screen.width;
+			//Draw the background of the merchant screen
+			GUI.Box(new Rect(0, 0, screenWidth, screenHeight), "");
+
+
+
+			//Draw the background for BUY/SELL/EXIT
+			GUI.Box (new Rect(0, 0, screenWidth * 0.295f, screenHeight * 0.285f), "BUY\nSELL\nEXIT");
+
+
+
+			//Draw the background for the Merchants Wares
+			GUI.BeginScrollView(new Rect(0, screenHeight*0.285f, screenWidth * 0.61f, screenHeight * 0.547f),
+
+			GUI.Box (, "");
+			float yPos = 5;
 			foreach(MerchantItem item in m_lItems)
 			{
-				GUI.Label(new Rect(0, yPos, 100, 18), item.m_szItemName);
+				GUI.Label(new Rect(5, screenHeight * 0.285f + yPos, 100, 18), item.m_szItemName);
 				yPos += 20;
 			}
+
+
+			//Draw the background for comparisson of what the characters currently have equipped
+			GUI.Box(new Rect(screenWidth*0.61f, screenHeight*0.285f, screenWidth * 0.391f, screenHeight*0.547f), "");
+
+
+
+			//Draw the background for the description of the item selected
+			GUI.Box(new Rect(0, screenHeight*0.832f, screenWidth, screenHeight * 0.166f), "");
+
+
+
+
+
+
 		}
 	}
 
