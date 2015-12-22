35ae154ab51b9b09d563d5cc12b0071ec741e648
Devan <devanoatney@gmail.com>
2015-12-22 12:04:44 -0600

    Added in a pathfinding prefab, currently just put it in Solomonia_Scene (as we don't use this scene) because I'm still testing it out and seeing how I'm going to get it to work with our scene data

---
+++ Game/Assets/Scripts/Editors/AStarPathfinding/PathfindingScript.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathfindingScript : MonoBehaviour {
+	
+	public Transform seeker, target;
+	
+	CGrid grid;
+	
+	void Awake() 
+	{
+		grid = GetComponent<CGrid>();
+	}
+	
+	void Update() 
+	{
+		FindPath(seeker.position,target.position);
+	}
+	
+	void FindPath(Vector3 startPos, Vector3 targetPos) 
+	{
+		CNode startNode = grid.NodeFromWorldPoint(startPos);
+		CNode targetNode = grid.NodeFromWorldPoint(targetPos);
+		
+		List<CNode> openSet = new List<CNode>();
+		HashSet<CNode> closedSet = new HashSet<CNode>();
+		openSet.Add(startNode);
+		
+		while (openSet.Count > 0) {
+			CNode currentNode = openSet[0];
+			for (int i = 1; i < openSet.Count; i ++) {
+				if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost) {
+					currentNode = openSet[i];
+				}
+			}
+			
+			openSet.Remove(currentNode);
+			closedSet.Add(currentNode);
+			
+			if (currentNode == targetNode) 
+			{
+				RetracePath(startNode,targetNode);
+				return;
+			}
+			
+			foreach (CNode neighbour in grid.GetNeighbours(currentNode)) 
+			{
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) 
+				{
+					continue;
+				}
+				
+				int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+				if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) 
+				{
+					neighbour.gCost = newMovementCostToNeighbour;
+					neighbour.hCost = GetDistance(neighbour, targetNode);
+					neighbour.parent = currentNode;
+					
+					if (!openSet.Contains(neighbour))
+						openSet.Add(neighbour);
+				}
+			}
+		}
+	}
+	
+	void RetracePath(CNode startNode, CNode endNode) 
+	{
+		List<CNode> path = new List<CNode>();
+		CNode currentNode = endNode;
+		
+		while (currentNode != startNode) 
+		{
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		
+		grid.path = path;
+		
+	}
+	
+	int GetDistance(CNode nodeA, CNode nodeB) 
+	{
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+		
+		if (dstX > dstY)
+			return 14*dstY + 10* (dstX-dstY);
+		return 14*dstX + 10 * (dstY-dstX);
+	}
+}
+
+
+
