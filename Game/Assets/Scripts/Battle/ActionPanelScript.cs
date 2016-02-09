a1e3a3cd36cd06a7604b2bbc5f7eee93a2cae561
Devan <devanoatney@gmail.com>
2016-02-09 13:48:47 -0600

    The new hexagonal action panel is implimented.

---
+++ Game/Assets/Scripts/Battle/ActionPanelScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActionPanelScript : MonoBehaviour 
+{
+	public int m_nActionIter;
+	TurnWatcherScript m_twTurnWatcher;
+	bool m_bIsHighlighted = false;
+	// Use this for initialization
+	void Start () 
+	{
+		m_twTurnWatcher = GameObject.Find("TurnWatcher").GetComponent<TurnWatcherScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0) && m_bIsHighlighted == true)
+		{
+			m_twTurnWatcher.ActionSelected(m_nActionIter);
+		}
+	}
+
+	void OnMouseEnter()
+	{
+		m_bIsHighlighted = true;
+		transform.parent.parent.GetComponent<CBattleActionsScript>().ChangeIndex(m_nActionIter);
+		transform.parent.FindChild("HoverImage").GetComponent<Image>().enabled = true;
+	}
+
+	void OnMouseExit()
+	{
+		m_bIsHighlighted = false;
+		transform.parent.FindChild("HoverImage").GetComponent<Image>().enabled = false;
+	}
+}
