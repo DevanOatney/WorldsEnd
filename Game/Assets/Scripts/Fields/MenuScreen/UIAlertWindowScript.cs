61b7a428c29fe8bb77b802028f991878e390c3f9
Devan <devanoatney@gmail.com>
2016-04-05 12:30:37 -0700

    Set up a new message window for when the player opens a treasure chest or recruits a new player.

---
+++ Game/Assets/Scripts/Fields/MenuScreen/UIAlertWindowScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIAlertWindowScript : MonoBehaviour 
+{
+	public enum MESSAGEID {eITEMREWARD, eJOINPARTY, eRECRUITED}
+	[SerializeField]
+	GameObject m_goItemRewardWindow = null;
+	[SerializeField]
+	GameObject m_goRecruitmentWindow = null;
+
+	private GameObject m_goListeningObject = null;
+	private bool m_bWindowActivated = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_bWindowActivated == true)
+		{
+			if(Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
+			{
+				m_bWindowActivated = false;
+				GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().ReleaseBind();
+				m_goItemRewardWindow.SetActive(false);
+				m_goRecruitmentWindow.SetActive(false);
+				if(m_goListeningObject != null)
+					m_goListeningObject.SendMessage("MessageWindowDeactivated");
+			}
+		}
+	}
+
+	//the listener can be null, in which case all that happens is that the message pops up, the player can't move until they press enter, and when they do the message dissappears.
+	public void ActivateWindow(MESSAGEID _nMessageID, string _szNameOfThing, GameObject _goListener)
+	{
+		GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().BindInput();
+		if(_nMessageID == MESSAGEID.eJOINPARTY)
+		{
+			m_goRecruitmentWindow.SetActive(true);
+			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szNameOfThing + " has joined the party!";
+			m_bWindowActivated = true;
+		}
+		else if(_nMessageID == MESSAGEID.eRECRUITED)
+		{
+			m_goRecruitmentWindow.SetActive(true);
+			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szNameOfThing + " has been recruited!";
+			m_bWindowActivated = true;
+		}
+		else if(_nMessageID == MESSAGEID.eITEMREWARD)
+		{
+			m_goItemRewardWindow.SetActive(true);
+			m_goItemRewardWindow.GetComponentInChildren<Text>().text = "Obtained " + _szNameOfThing + ".";
+			m_bWindowActivated = true;
+		}
+		m_goListeningObject = _goListener;
+	}
+
+}
--- Game/Assets/Scripts/Fields/TreasureChestScript.cs
+++ Game/Assets/Scripts/Fields/TreasureChestScript.cs
@@ -13,25 +13,18 @@ public class TreasureChestScript : MonoBehaviour {
 	public void SetItemHeld(ItemLibrary.CharactersItems item){m_ciItemHeld = item;}
 	//the amount of the item held within to give to the player
 	public int m_nAmountofItem = 1;
-	Vector2 m_vSizeOfMessage;
 
-	Vector3 positionOfPlayer = new Vector3();
-
-	float m_fBoxOpacity = 0.0f;
-	bool m_bMessagePopped = false;
 	//Temp for testing
 	public string m_szItemName;
 	public AudioClip m_acOpeningSound;
 	// Use this for initialization
 	void Start () 
 	{
-		//TEMP FOR TESTING
 		m_ciItemHeld = new ItemLibrary.CharactersItems();
 		m_ciItemHeld.m_szItemName = m_szItemName;
-		m_ciItemHeld.m_nItemCount = Random.Range(1, 4);
+		m_ciItemHeld.m_nItemCount = m_nAmountofItem;
 
 		m_fAnimLength = m_acOpeningAnim.length;
-		m_vSizeOfMessage = new Vector2((m_ciItemHeld.m_szItemName.Length + 10)* 15 , 25);
 		int result;
 		if(GameObject.Find("PersistantData").GetComponent<DCScript>().m_dStoryFlagField.TryGetValue(name, out result))
 		{
@@ -44,11 +37,6 @@ public class TreasureChestScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKeyUp(KeyCode.Return) && m_bIsOpened == true)
-		{
-			GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().ReleaseBind();
-			m_bMessagePopped = true;
-		}
 		if(m_bIsOpening == true && m_bIsOpened == false)
 		{
 			m_fAnimLength -= Time.deltaTime;
@@ -65,6 +53,8 @@ public class TreasureChestScript : MonoBehaviour {
 					GameObject.Find("PersistantData").GetComponent<DCScript>().m_nGold += m_ciItemHeld.m_nItemCount;
 					GameObject.Find("PersistantData").GetComponent<DCScript>().m_dStoryFlagField.Add(name, 1);
 				}
+				string szMessage = m_ciItemHeld.m_nItemCount + " " + m_ciItemHeld.m_szItemName;
+				GameObject.Find("UI_Alerts").GetComponent<UIAlertWindowScript>().ActivateWindow(UIAlertWindowScript.MESSAGEID.eITEMREWARD, szMessage, null);
 			}
 		}
 	}
@@ -79,34 +69,9 @@ public class TreasureChestScript : MonoBehaviour {
 				if(chestAnim.GetBool("m_bIsOpening") == false)
 					chestAnim.SetBool("m_bIsOpening", true);
 				m_bIsOpening = true;
-				GetComponent<AudioSource>().PlayOneShot(m_acOpeningSound, 0.5f + GameObject.Find("PersistantData").GetComponent<DCScript>().m_fSFXVolume);
-				GameObject player = c.GetComponent<InterractingBoxScript>().GetOwner();
-				player.GetComponent<FieldPlayerMovementScript>().BindInput();
-				positionOfPlayer = player.transform.position;
-				positionOfPlayer.x = player.GetComponent<BoxCollider2D>().bounds.min.x;
-				positionOfPlayer.y += player.GetComponent<BoxCollider2D>().bounds.size.y * 0.9f;
-				m_fBoxOpacity = 0.1f;
-				Input.ResetInputAxes();
+				if(m_acOpeningSound != null)
+					GetComponent<AudioSource>().PlayOneShot(m_acOpeningSound, 0.5f + GameObject.Find("PersistantData").GetComponent<DCScript>().m_fSFXVolume);
 			}
-		}
-	}
-
-	void OnGUI()
-	{
-		if(m_bIsOpened == true && m_bMessagePopped == false)
-		{
-			m_fBoxOpacity +=  5 * Time.deltaTime;
-			if(m_fBoxOpacity > 0.95f)
-				m_fBoxOpacity = 1.0f;
-			GUI.color = new Color(1.0f, 1.0f, 1.0f, m_fBoxOpacity); //0.5 is half opacity 
-			Vector2 pos = Camera.main.WorldToScreenPoint(positionOfPlayer);
-			pos.x = (pos.x - m_vSizeOfMessage.x * 0.5f) + GameObject.Find("Player").GetComponent<BoxCollider2D>().bounds.size.x;
-			pos.y = Screen.height - pos.y;
-			GUI.Box(new Rect(pos.x, pos.y, m_vSizeOfMessage.x +10, m_vSizeOfMessage.y), "You have found " + m_nAmountofItem + " " + m_ciItemHeld.m_szItemName);
-			GUI.color = Color.white;
-
-			if(m_fBoxOpacity <= 0.1f)
-				m_bMessagePopped = true;
 		}
 	}
 }
