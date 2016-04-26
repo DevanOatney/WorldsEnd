14979754cc16a7c4e5cc95696f0465507460e4df
Devan <devanoatney@gmail.com>
2016-04-26 13:05:28 -0700

    Set it up so that you can queue alerts for multiple item rewards and party recruitments.

--- Game/Assets/Scripts/Fields/MenuScreen/UIAlertWindowScript.cs
+++ Game/Assets/Scripts/Fields/MenuScreen/UIAlertWindowScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class UIAlertWindowScript : MonoBehaviour 
@@ -12,7 +13,13 @@ public class UIAlertWindowScript : MonoBehaviour
 
 	private GameObject m_goListeningObject = null;
 	private bool m_bWindowActivated = false;
-
+	public class cKVP
+	{
+		public cKVP(MESSAGEID _ID, string _msg) {_eID = _ID; _szMessage = _msg;}
+		public MESSAGEID _eID;
+		public string _szMessage;
+	}
+	Queue<cKVP> m_qMessages = new Queue<cKVP>();
 	// Use this for initialization
 	void Start () 
 	{
@@ -26,12 +33,19 @@ public class UIAlertWindowScript : MonoBehaviour
 		{
 			if(Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
 			{
-				m_bWindowActivated = false;
-				GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().ReleaseBind();
-				m_goItemRewardWindow.SetActive(false);
-				m_goRecruitmentWindow.SetActive(false);
-				if(m_goListeningObject != null)
-					m_goListeningObject.SendMessage("MessageWindowDeactivated", SendMessageOptions.RequireReceiver);
+				if(m_qMessages.Count <= 0)
+				{
+					m_bWindowActivated = false;
+					GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().ReleaseBind();
+					m_goItemRewardWindow.SetActive(false);
+					m_goRecruitmentWindow.SetActive(false);
+					if(m_goListeningObject != null)
+						m_goListeningObject.SendMessage("MessageWindowDeactivated", SendMessageOptions.RequireReceiver);
+				}
+				else
+				{
+					SetUpMessageWindow(m_qMessages.Dequeue());
+				}
 			}
 		}
 	}
@@ -39,26 +53,46 @@ public class UIAlertWindowScript : MonoBehaviour
 	//the listener can be null, in which case all that happens is that the message pops up, the player can't move until they press enter, and when they do the message dissappears.
 	public void ActivateWindow(MESSAGEID _nMessageID, string _szNameOfThing, GameObject _goListener)
 	{
-		GameObject.Find("Player").GetComponent<FieldPlayerMovementScript>().BindInput();
-		if(_nMessageID == MESSAGEID.eJOINPARTY)
+		if(m_qMessages.Count > 0)
 		{
-			m_goRecruitmentWindow.SetActive(true);
-			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szNameOfThing + " has joined the party!";
-			m_bWindowActivated = true;
+			m_qMessages.Enqueue(new cKVP(_nMessageID, _szNameOfThing));
 		}
-		else if(_nMessageID == MESSAGEID.eRECRUITED)
+		else
 		{
-			m_goRecruitmentWindow.SetActive(true);
-			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szNameOfThing + " has been recruited!";
-			m_bWindowActivated = true;
+			GameObject _player = GameObject.Find("Player");
+			if(_player.GetComponent<FieldPlayerMovementScript>().GetAllowInput() == false)
+				_player.GetComponent<FieldPlayerMovementScript>().BindInput();
+			SetUpMessageWindow(_nMessageID, _szNameOfThing);
 		}
-		else if(_nMessageID == MESSAGEID.eITEMREWARD)
-		{
-			m_goItemRewardWindow.SetActive(true);
-			m_goItemRewardWindow.GetComponentInChildren<Text>().text = "Obtained " + _szNameOfThing + ".";
-			m_bWindowActivated = true;
-		}
+
+
 		m_goListeningObject = _goListener;
 	}
 
+	void SetUpMessageWindow(cKVP _msg)
+	{
+		SetUpMessageWindow(_msg._eID, _msg._szMessage);
+	}
+	
+	void SetUpMessageWindow(MESSAGEID _eID, string _szMessage)
+	{
+		if(_eID == MESSAGEID.eJOINPARTY)
+		{
+			m_goRecruitmentWindow.SetActive(true);
+			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szMessage + " has joined the party!";
+			m_bWindowActivated = true;
+		}
+		else if(_eID == MESSAGEID.eRECRUITED)
+		{
+			m_goRecruitmentWindow.SetActive(true);
+			m_goRecruitmentWindow.GetComponentInChildren<Text>().text = _szMessage + " has been recruited!";
+			m_bWindowActivated = true;
+		}
+		else if(_eID == MESSAGEID.eITEMREWARD)
+		{
+			m_goItemRewardWindow.SetActive(true);
+			m_goItemRewardWindow.GetComponentInChildren<Text>().text = "Obtained " + _szMessage + ".";
+			m_bWindowActivated = true;
+		}
+	}
 }
--- Game/Assets/Scripts/Fields/PortraitContainerScript.cs
+++ Game/Assets/Scripts/Fields/PortraitContainerScript.cs
@@ -7,13 +7,16 @@ public class PortraitContainerScript : MonoBehaviour
 	public Dictionary<string,Sprite> m_dPortraits = new Dictionary<string, Sprite>();
 	public List<Sprite> m_lBriolPortraits;
 	public List<Sprite> m_lCallanPortraits;
+	public List<Sprite> m_lMattachPortraits;
 	// Use this for initialization
 	void Start () 
 	{
 		for(int i = 0; i < m_lBriolPortraits.Count; ++i)
 			m_dPortraits.Add("Briol"+i.ToString(), m_lBriolPortraits[i]);
 		for(int i = 0; i < m_lCallanPortraits.Count; ++i)
-			m_dPortraits.Add("Callan"+i.ToString(), m_lCallanPortraits[i]);	             
+			m_dPortraits.Add("Callan"+i.ToString(), m_lCallanPortraits[i]);
+		for(int i = 0; i < m_lMattachPortraits.Count; ++i)
+			m_dPortraits.Add("Mattach"+i.ToString(), m_lMattachPortraits[i]);
 	}
 	
 	// Update is called once per frame
