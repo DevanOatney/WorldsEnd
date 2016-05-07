8ecd274b7ff6244d4194e5855af396baddbad47f
Devan <devanoatney@gmail.com>
2016-05-07 04:02:16 -0700

    Began to add in the ground work for war battles using the PLACEHOLDER unit assets from Suikoden II (Need to get our own before Kickstarter obviously)

---
+++ Game/Assets/Scripts/WarBattle/WB_UnitScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WB_UnitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
