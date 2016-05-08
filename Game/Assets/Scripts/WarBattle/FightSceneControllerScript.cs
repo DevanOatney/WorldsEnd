7be1187bdb8fd7530800b8051c13eaf82efc9293
Devan <devanoatney@gmail.com>
2016-05-08 01:56:56 -0700

    Set up a controller for the fight scene window in war battles so that each sides forces spawn. Very basic right now, input tests forces spawning, charging, and the fight scene ending.

---
+++ Game/Assets/Scripts/WarBattle/FightSceneControllerScript.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FightSceneControllerScript : MonoBehaviour 
+{
+	public class cWarUnit
+	{
+		public cWarUnit(GameObject _ldr, GameObject _unit, float _percent, int totalCount) {m_goLeaderSprite = _ldr; m_goSprite = _unit; m_fPercentRemaining = _percent; m_nTotalCount = totalCount;}
+		public GameObject m_goLeaderSprite;
+		public GameObject m_goSprite;
+		public float m_fPercentRemaining;
+		public int m_nTotalCount;
+	}
+
+	public GameObject m_goLeftSide, m_goRightSide, m_goLowerBound;
+	List<GameObject> m_lLeftUnits = new List<GameObject>();
+	List<GameObject> m_lRightUnits = new List<GameObject>();
+	float m_fUnitYOffset = 15.0f;
+	float m_fUnitXOffset = 15.0f;
+
+
+	/// TEMP STUFF TO TEST
+	public GameObject TEMP_goUnit;
+
+	void TEMP_BuildTestBattle()
+	{
+		cWarUnit leftUnit = new cWarUnit(null, TEMP_goUnit, 1.0f, 10);
+		cWarUnit rightUnit = new cWarUnit(null, TEMP_goUnit, 1.0f, 15);
+		SetupBattleScene(leftUnit, rightUnit);
+
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TEMP_BuildTestBattle();
+		if(Input.GetKeyDown(KeyCode.Escape))
+			End();
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			foreach(GameObject _unit in m_lLeftUnits)
+				_unit.GetComponent<WB_UnitScript>().TimeToMove();
+			foreach(GameObject _unit in m_lRightUnits)
+				_unit.GetComponent<WB_UnitScript>().TimeToMove();
+		}
+	}
+
+	void End()
+	{
+		foreach(GameObject _unit in m_lLeftUnits)
+			Destroy(_unit);
+		foreach(GameObject _unit in m_lRightUnits)
+			Destroy(_unit);
+		m_lLeftUnits.Clear();
+		m_lRightUnits.Clear();
+	}
+
+	public void SetupBattleScene(cWarUnit _leftSide, cWarUnit _rightSide)
+	{
+		m_lLeftUnits.Clear();
+		m_lRightUnits.Clear();
+		LoadInASide(1, _leftSide);
+		LoadInASide(-1, _rightSide);
+	}
+
+	//-1 for if it's on the right, 1 for if it's on the left
+	void LoadInASide(int _side, cWarUnit _unit)
+	{
+		float _fXOffset = 0.0f, _fYOffset = 0.0f;
+		int _forceRemaining = (int)(_unit.m_nTotalCount * _unit.m_fPercentRemaining);
+		if(_forceRemaining == 0)
+			_forceRemaining = 1;
+		int _amountOfDead = _unit.m_nTotalCount - _forceRemaining;
+		if(_forceRemaining == 1)
+		{
+			if(_unit.m_goLeaderSprite == null)
+			{
+				//if this unit group doesn't have a leader and is down to their last unit, only spawn 1 basic unit.
+			}
+			else
+			{
+				//If the unit is down to it's last person and you do have a leader, only spawn the leader and call it good.
+			}
+		}
+		int _nDeadSoFar = 0;
+		for(int i = 0; i < _forceRemaining; ++i)
+		{
+			Vector3 _startPos = Vector3.zero;
+			GameObject _char = null;
+			if(i+1 == _forceRemaining && _unit.m_goLeaderSprite != null)
+				_char = Instantiate(_unit.m_goLeaderSprite) as GameObject;
+			else
+			    _char = Instantiate(_unit.m_goSprite) as GameObject;
+			_char.transform.SetParent(gameObject.transform);
+			_char.transform.localScale = Vector3.one;
+			if(_forceRemaining + _nDeadSoFar < _unit.m_nTotalCount)
+			{
+				//So if we're in here, there's still a chance to spawn a dead unit.
+				int _nGap = _unit.m_nTotalCount - (_forceRemaining + _nDeadSoFar);
+				if(_nGap < i+2)
+				{
+					//So in here it means that by the next iteration, there won't be enough units left to initialize to represent all of the dead units, so it has to be dead.
+					_char.GetComponent<WB_UnitScript>().Initialize(m_goLeftSide, m_goRightSide, _side, true);
+				}
+				else
+				{
+					//We've still got time, so roll the dice!
+					bool rndm = ShouldThisOneBeDead();
+					if(rndm == true)
+					{
+						_nDeadSoFar += 1;
+						_char.GetComponent<WB_UnitScript>().Initialize(m_goLeftSide, m_goRightSide, _side, true);
+					}
+					else
+						_char.GetComponent<WB_UnitScript>().Initialize(m_goLeftSide, m_goRightSide, _side, false);
+				}
+			}
+			else
+				_char.GetComponent<WB_UnitScript>().Initialize(m_goLeftSide, m_goRightSide, _side, false);
+		
+			if(_side == 1)
+			{
+				_startPos = m_goLeftSide.transform.localPosition;
+				_char.transform.localPosition = AdjustPositionOfUnit(_startPos, ref _fXOffset, ref _fYOffset);
+
+				Vector3 _scale = _char.transform.localScale;
+				_scale.x *= -1;
+				_char.transform.localScale = _scale;
+				m_lLeftUnits.Add(_char);
+			}
+			else
+			{
+				_startPos = m_goRightSide.transform.localPosition;
+				_char.transform.localPosition = AdjustPositionOfUnit(_startPos, ref _fXOffset, ref _fYOffset);
+				m_lRightUnits.Add(_char);
+			}
+			_fYOffset += m_fUnitYOffset;
+		}
+
+	}
+
+	bool ShouldThisOneBeDead()
+	{
+		int _nCoinFlip = Random.Range(0, 1);
+		if(_nCoinFlip == 0)
+			return false;
+		return true;
+	}
+	Vector3 AdjustPositionOfUnit(Vector3 _startPos, ref float _fXOffset, ref float _fYOffset)
+	{
+		if(_startPos.y - _fYOffset <= m_goLowerBound.transform.localPosition.y)
+		{
+			_fYOffset = 0.0f;
+			_fXOffset += m_fUnitXOffset;
+		}
+		_startPos.x += _fXOffset;
+		_startPos.y -= _fYOffset;
+		return _startPos;
+	}
+}
--- Game/Assets/Scripts/WarBattle/WB_UnitScript.cs
+++ Game/Assets/Scripts/WarBattle/WB_UnitScript.cs
@@ -1,15 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WB_UnitScript : MonoBehaviour {
+public class WB_UnitScript : MonoBehaviour 
+{
+	//Flags
+	bool m_bStartDeath = false;
+	[HideInInspector]
+	public  bool m_bHasDied = false;
+	bool m_bShouldAttack = false;
+	bool m_bShouldMove = false;
+	//Stats
+	public float m_fMovementSpeed = 5.0f;
+	//-1 to move left, +1 to move right
+	int m_nDirection = -1;
+	//Hooks
+	public GameObject m_goDeathFlash;
+	public GameObject m_goLeftBound, m_goRightBound;
+
+	public void Initialize(GameObject _leftBound, GameObject _rightBound, int _nDirection, bool _amIDead)
+	{
+		m_goLeftBound = _leftBound;
+		m_goRightBound = _rightBound;
+		m_nDirection = _nDirection;
+		if(_amIDead == true)
+		{
+			m_bStartDeath = true;
+			m_bHasDied = true;
+			GetComponent<Animator>().SetBool("m_bAlreadyDead", true);
+		}
+	}
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//Check to see if we're in the "swinging zone" and if we haven't started swinging, swing.  If we leave the swinging zone, stop swinging
+
+		//Check if we should move, if we should, do it until we get to the end location
+		if(m_bStartDeath == false && m_bShouldMove == true)
+		{
+			if(m_nDirection == -1)
+			{
+				if(transform.localPosition.x <= m_goLeftBound.transform.localPosition.x)
+					ReachedDestination();
+			}
+			else
+			{
+				if(transform.localPosition.x >= m_goRightBound.transform.localPosition.x)
+					ReachedDestination();
+			}
+			float dir = m_nDirection * m_fMovementSpeed * Time.deltaTime;
+			Vector3 vecDir = new Vector3(m_nDirection*m_fMovementSpeed*Time.deltaTime, 0, 0);
+			GetComponent<RectTransform>().Translate(vecDir);
+		}
+
+	}
+
+	void ReachedDestination()
+	{
+		m_bShouldMove = false;
+	}
+
+	public void TimeToMove()
+	{
+		if(m_bHasDied == false)
+			m_bShouldMove = true;
+	}
+
+	public void TimeToAttack()
+	{
+		if(m_bHasDied == false)
+			GetComponent<Animator>().SetBool("m_bAttack", true);
+	}
+
+	public void TimeToDie()
+	{
+		GetComponent<Animator>().SetBool("m_bDie", true);
+	}
+
+	public void HasDied()
+	{
+		m_bHasDied = true;
 	}
 }
