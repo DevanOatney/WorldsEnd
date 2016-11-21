01e9439c460ff3fb38d3f19226ece54539b082b7
Devan <devanoatney@gmail.com>
2016-11-21 00:51:48 -0800

    Finished the coding for changing what kind of encounters you can encounter in the world map based on your position :)
    Just need to have someone actually go in and set those boundaries.

--- Game/Assets/Scripts/WorldMap/EncounterBoundaryScript.cs
+++ Game/Assets/Scripts/WorldMap/EncounterBoundaryScript.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EncounterBoundaryScript : MonoBehaviour {
-
+public class EncounterBoundaryScript : MonoBehaviour 
+{
+	public TextAsset[] m_goEncounters;
+	GameObject m_goWatcher;
 	// Use this for initialization
 	void Start () {
+		m_goWatcher = GameObject.Find ("OverWatcher");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.name == "Player")
+		{
+			m_goWatcher.GetComponent<EncounterGroupLoaderScript> ().m_szEncounters = m_goEncounters;
+		}
 	}
 }
